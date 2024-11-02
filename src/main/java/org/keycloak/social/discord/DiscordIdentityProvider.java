c10480b79864a85817d20d48f949475020322090
Andy Thorne <andythorne@users.noreply.github.com>
2024-11-02 04:27:47 +0000

    feat: updated provider for Keycloak 25.x (#49)

--- src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
+++ src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
@@ -67,11 +67,10 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
 
     @Override
     protected BrokeredIdentityContext extractIdentityFromProfile(EventBuilder event, JsonNode profile) {
-        BrokeredIdentityContext user = new BrokeredIdentityContext(getJsonProperty(profile, "id"));
+        BrokeredIdentityContext user = new BrokeredIdentityContext(getJsonProperty(profile, "id"), getConfig());
 
         user.setUsername(getJsonProperty(profile, "username") + "#" + getJsonProperty(profile, "discriminator"));
         user.setEmail(getJsonProperty(profile, "email"));
-        user.setIdpConfig(getConfig());
         user.setIdp(this);
 
         AbstractJsonUserAttributeMapper.storeUserProfileForMapper(user, profile, getConfig().getAlias());
